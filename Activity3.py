1dca76e45b24ecd421bc08a4e1dbe984bda05355
tahjibinfo-design <tahjibinfo@gmai.com>
2026-06-12 21:30:19 +0600

    Tahib

---
+++ Activity3.py
@@ -0,0 +1,11 @@
+print ("Enter your Marks out of 100")
+
+math = int(input(Maths mark: ))
+english = int(input(English mark: ))
+science = int(input(Science mark: ))
+
+total = math + english + science
+print ("Your total marks is:" , total)
+
+percentage = (total/300)*100
+print ("Your percentage mark is:" , percentage)
