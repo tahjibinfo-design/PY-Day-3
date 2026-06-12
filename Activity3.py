535e53997c648c0c40649e4a9299f91f35c0309e
tahjibinfo-design <tahjibinfo@gmai.com>
2026-06-12 21:32:53 +0600

    Tahjib

--- Activity3.py
+++ Activity3.py
@@ -1,8 +1,8 @@
 print ("Enter your Marks out of 100")
 
-math = int(input(Maths mark: ))
-english = int(input(English mark: ))
-science = int(input(Science mark: ))
+math = int(input("Maths mark: "))
+english = int(input("English mark: "))
+science = int(input("Science mark: "))
 
 total = math + english + science
 print ("Your total marks is:" , total)
