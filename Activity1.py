97ac6bb008fe0133f3d84d62d9f4330bcb3ebd50
tahjibinfo-design <tahjibinfo@gmai.com>
2026-06-12 21:04:54 +0600

    Tahjib

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+boy1 = 175
+boy2 = 150
+boy3 = 140
+boy4 = 161
+boy5 = 136
+
+Sum = boy1 + boy2 + boy3 + boy4 + boy5
+print ("The sum of the height of the boys is " , Sum , "cm")
+
+average = Sum/5
+print ("The average height of the boys is " , average , "cm")
